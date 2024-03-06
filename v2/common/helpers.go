91011e644354bb9b9efecec9a7f38cbf8009f6bb
kingwel.xie@139.com <kingwel.xie@139.com>
2024-03-06 17:55:19 +0800

    update: LotSize workaround deleted

--- v2/common/helpers.go
+++ v2/common/helpers.go
@@ -8,8 +8,7 @@ import (
 
 // AmountToLotSize converts an amount to a lot sized amount
 func AmountToLotSize(lot float64, precision int, amount float64) float64 {
-	//  + lot * 0.001, workaround of float precision issue
-	return math.Trunc(math.Floor((amount+lot*0.001)/lot)*lot*math.Pow10(precision)) / math.Pow10(precision)
+	return math.Trunc(math.Floor(amount/lot)*lot*math.Pow10(precision)) / math.Pow10(precision)
 }
 
 // ToJSONList convert v to json list if v is a map
